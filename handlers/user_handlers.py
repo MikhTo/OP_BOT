0c8d86f2edbb7489f201c4139bf8d86fb6b91184
Mikhail Tolkachev <m.tolkachev@g.nsu.ru>
2023-02-09 09:46:52 +0700

    material's sending is fixed

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,10 +1,12 @@
 from aiogram import Dispatcher
 
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, MediaGroup
 
 from lexicon.lexicon import LEXICON_RU
 
 from keyboard.keyboards import get_inline_markup, get_simple_markup
+
+import os, shutil
 
 ADMIN_ID=829365853
 
@@ -33,12 +35,34 @@ async def process_accept_student_press(callback: CallbackQuery):
 
 async def process_materials_press(message: Message):
     list_of_sems: list[str] = get_seminars_list(LEXICON_RU["materials_directory"])
-    await message.answer(text=LEXICON_RU["choose_seminars"], reply_markup=get_inline_markup(1, *list_of_sems, "all_sem"))
+    material_kb: types.InlineKeyboardMarkup = get_inline_markup(1, "all_sem", *list_of_sems)
+    await message.answer(text=LEXICON_RU["choose_seminars"], reply_markup=material_kb)
 
 async def process_sem_press(callback: CallbackQuery):
-    file_bytes = get_materials(callback.data, LEXICON_RU["materials_directory"])
+    path=LEXICON_RU["materials_directory"]
+    material = callback.data
+    media = types.MediaGroup()
     await callback.message.delete()
-    await callback.message.answer_document(document=file_bytes,caption="⬆️"+LEXICON_RU[callback.data],)
+    non_sem_key: list[str] = ["all_sem", "tasks_list.pdf", "book.pdf"]
+    file_list= []
+    if material not in non_sem_key:
+        path+=f"/{material}"
+        file_list= [open(path+"/"+f, "rb") for f in os.listdir(path)]
+        for i, f in enumerate(file_list):
+            ff = os.listdir(path)[i]
+            media.attach_document(f,
+                                  caption="код" if not ".pdf" in ff else "презентация")
+        await callback.message.answer_media_group(media=media)
+        for f in file_list:
+            f.close()
+    elif material == "all_sem":
+        shutil.make_archive(material, 'zip', path)
+        with open(material+".zip", "rb") as zip_f:
+            await callback.message.answer_document(document=zip_f, caption="⬆️"+LEXICON_RU[callback.data])
+        os.remove(material+".zip")
+    else:
+        with open(path+"/"+material, "rb") as mat:
+            await callback.message.answer_document(document=mat, caption="⬆️"+LEXICON_RU[callback.data])
 
 async def process_start_new_command(message: Message):
     await message.answer(text=f"ваша заявка отправлена! \n Ожидайте подтверждения от преподавателя")
@@ -46,6 +70,7 @@ async def process_start_new_command(message: Message):
                             reply_markup=get_inline_markup(2, accept=f"accept: {message.from_user.id}", 
                                                            deny=f"deny: {message.from_user.id}", 
                                                            accept_guest=f"accept_guest: {message.from_user.id}"))
+                                                           
 
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU["greetings"], parse_mode="HTML")
@@ -58,4 +83,4 @@ def register_user_handlers(dp: Dispatcher):
     dp.register_message_handler(process_help_command, commands=["help"])
     dp.register_message_handler(process_materials_press, commands=["materials"])
 
-    dp.register_callback_query_handler(process_sem_press, lambda data: "sem" in data.data)
+    dp.register_callback_query_handler(process_sem_press, lambda data: "sem" in data.data or "book.pdf" in data.data or "tasks_list.pdf")
--- services/file_handling.py
+++ services/file_handling.py
@@ -1,20 +1,26 @@
 import os
 import shutil
-
+from aiogram import types
 def get_seminars_list(path: str|None = None) -> list[str]:
-    return os.listdir(path)
+    return sorted(os.listdir(path), key=lambda item:"sem" in item, reverse=True)
 
-def get_materials(material: str, path: str = './') -> bytes:
-
-    if material != "all_sem":
+def get_materials(material: str, path: str = './') -> types.MediaGroup:
+    media = types.MediaGroup()
+    non_sem_key: list[str] = ["all_sem", "tasks_list", "book"]
+    if material not in non_sem_key:
         path+=f"/{material}"
-    shutil.make_archive(material, 'zip', path)
-    file_bytes: bytes
-    with open(material+".zip", "rb") as zip_f:
-        file_bytes = zip_f.read()
-    os.remove(material+".zip")
-    return file_bytes
+        for f in os.listdir(path):
+            with open(path+"/"+f, "rb") as doc:
+                media.attach_document(doc)
+    elif material == "all_sem":
+        shutil.make_archive(material, 'zip', path)
+        with open(material+".zip", "rb") as zip_f:
+            media.attach_document(zip_f)
+        os.remove(material+".zip")
+    else:
+        pass
 
+    return media
 if __name__ == "__main__":
     res = get_seminars_list("./services/materials")#r
     res=get_materials("all","./services/materials")
